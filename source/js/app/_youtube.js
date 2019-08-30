180e6abb0a6cc339450808a0e66d0985d76bb9af
Hemerson Vianna <hemerson.lourenco@gmail.com>
2019-08-30 01:36:27 -0300

    feat: add youtube script

---
+++ source/js/app/_youtube.js
@@ -0,0 +1,24 @@
+const youtube = document.querySelectorAll( ".youtube" );
+
+if (youtube.length > 0) {
+  for (var i = 0; i < youtube.length; i++) {
+    const source = "https://img.youtube.com/vi/"+ youtube[i].dataset.embed +"/hqdefault.jpg";
+
+    const image = new Image();
+    image.src = source;
+    image.addEventListener( "load", function() {
+      youtube[ i ].appendChild( image );
+    }(i));
+
+    youtube[i].addEventListener( "click", function() {
+      var iframe = document.createElement( "iframe" );
+      iframe.setAttribute( "frameborder", "0" );
+      iframe.setAttribute( "allowfullscreen", "" );
+      iframe.setAttribute( "allow", "autoplay");
+      iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ this.dataset.embed +"?rel=0&showinfo=0&autoplay=1" );
+
+      this.innerHTML = "";
+      this.appendChild( iframe );
+    } );
+  }
+}
