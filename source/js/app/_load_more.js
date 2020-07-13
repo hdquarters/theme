eebd69b09891e353e0980fccea66470ba5ecd90b
Hemerson Vianna <hemerson.lourenco@gmail.com>
2020-07-13 04:28:29 -0300

    feat: update cards

--- source/js/app/_load_more.js
+++ source/js/app/_load_more.js
@@ -1,53 +1,56 @@
 export function loadMore($container, item) {
-  const $items     = $container.querySelectorAll(item);
-  const $button    = $container.querySelector('.js-load_more');
-  const itemsSize  = $items.length;
-  let x            = 9;
+  const $items = $container.querySelectorAll(item);
+  const $button = $container.querySelector(".js-load_more");
+  const itemsSize = $items.length;
+  let x = 9;
 
-  [].forEach.call($items, function(element, index) {
-    if( index < x ){
-      element.style.display = "block";
+  [].forEach.call($items, function (element, index) {
+    if (index < x) {
+      element.style.display = "flex";
     }
   });
 
-  if($button){
-    if(x < itemsSize) {
-      $button.style.display = "block";
+  if ($button) {
+    if (x < itemsSize) {
+      $button.style.display = "flex";
     } else {
       $button.style.display = "none";
     }
 
     $button.addEventListener("click", function () {
-      x = (x + 9 < itemsSize) ? x + 9 : itemsSize;
+      x = x + 9 < itemsSize ? x + 9 : itemsSize;
 
-      [].forEach.call($items, function(element, index) {
-        if( index < x ){
-          element.style.display = "block";
+      [].forEach.call($items, function (element, index) {
+        if (index < x) {
+          element.style.display = "flex";
         }
       });
 
-      if(x === itemsSize){
+      if (x === itemsSize) {
         $button.style.display = "none";
       }
     });
   }
-};
+}
 
 export function filter() {
-  if( document.querySelector('.js-shield_button') ){
-    const $buttons = document.querySelectorAll('.js-shield_button');
+  if (document.querySelector(".js-shield_button")) {
+    const $buttons = document.querySelectorAll(".js-shield_button");
 
-    [].forEach.call($buttons, function(element, index) {
-      element.addEventListener("click", function(){
+    [].forEach.call($buttons, function (element, index) {
+      element.addEventListener("click", function () {
         let shield = this.getAttribute("data-shield");
-        const $filters = document.querySelectorAll('.filter-item');
+        const $filters = document.querySelectorAll(".filter-item");
 
-        [].forEach.call($filters, function(element, index) {
+        [].forEach.call($filters, function (element, index) {
           element.style.display = "none";
         });
 
-        loadMore(document.querySelector('.filter'), '.filter-item[data-shield='+shield+']');
+        loadMore(
+          document.querySelector(".filter"),
+          ".filter-item[data-shield=" + shield + "]"
+        );
       });
     });
   }
-};
+}
