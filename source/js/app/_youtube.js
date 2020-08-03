f0a91ca2c8e5cc18f67d7d9c369c2caa263bdf82
Hemerson Vianna <hemerson.lourenco@gmail.com>
2020-08-03 18:56:50 -0300

    feat: update youtube script

--- source/js/app/_youtube.js
+++ source/js/app/_youtube.js
@@ -1,24 +1,47 @@
-const youtube = document.querySelector('body').querySelectorAll(".youtube");
+const youtube = document.querySelector("body").querySelectorAll(".youtube");
 
 if (youtube.length > 0) {
   for (var i = 0; i < youtube.length; i++) {
-    const source = "https://img.youtube.com/vi/"+ youtube[i].dataset.embed +"/hqdefault.jpg";
+    const source =
+      "https://img.youtube.com/vi/" +
+      youtube[i].dataset.embed +
+      "/hqdefault.jpg";
 
     const image = new Image();
     image.src = source;
-    image.addEventListener( "load", function() {
-      youtube[ i ].appendChild( image );
-    }(i));
+    image.addEventListener(
+      "load",
+      (function () {
+        youtube[i].appendChild(image);
+      })(i)
+    );
 
-    youtube[i].addEventListener( "click", function() {
-      var iframe = document.createElement( "iframe" );
-      iframe.setAttribute( "frameborder", "0" );
-      iframe.setAttribute( "allowfullscreen", "" );
-      iframe.setAttribute( "allow", "autoplay");
-      iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ this.dataset.embed +"?rel=0&showinfo=0&autoplay=1" );
-
+    youtube[i].addEventListener("click", function () {
+      var iframe = document.createElement("iframe");
+      iframe.setAttribute("frameborder", "0");
+      iframe.setAttribute("allowfullscreen", "");
+      if (!!this.dataset.playlist) {
+        iframe.setAttribute(
+          "allow",
+          "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        );
+        iframe.setAttribute(
+          "src",
+          "https://www.youtube.com/embed/videoseries?list=" +
+            this.dataset.playlist +
+            "&autoplay=1"
+        );
+      } else {
+        iframe.setAttribute("allow", "autoplay");
+        iframe.setAttribute(
+          "src",
+          "https://www.youtube.com/embed/" +
+            this.dataset.embed +
+            "?rel=0&showinfo=0&autoplay=1"
+        );
+      }
       this.innerHTML = "";
-      this.appendChild( iframe );
-    } );
+      this.appendChild(iframe);
+    });
   }
 }
